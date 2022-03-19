0a550bea9d08ea436532b4fb745537b6d66a74aa
mithin888 <mithingthomas@gmail.com>
2022-03-18 21:09:53 -0400

    initial commit

---
+++ notes.js
@@ -0,0 +1,14 @@
+// const firstName = require("./utils");
+
+// // const name = "Mithin";
+
+// console.log(firstName);
+
+const add = require("./utils.js");
+
+const sum = add(4, -2);
+console.log(sum);
+
+const getNotes = require("./notes.js");
+const msg = getNotes();
+console.log(msg);
